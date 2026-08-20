a6de3666e9c3a0bc43226ce39bd3e26fd3032541
Boddu Pardha Saradhi <2300030087@kluniversity.in>
2026-08-20 23:45:34 +0530

    Time: 24 ms (87.27%), Space: 106.8 MB (61.4%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) 
+    {
+        int n = temperatures.length;
+
+        Deque<Integer> st = new ArrayDeque<>();
+        int ans[] =new int[n];
+
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && temperatures[i]>=temperatures[st.peek()])
+            {
+                st.pop();
+            }
+
+            if(!st.isEmpty())
+            {
+                 ans[i]=st.peek()-i;
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+
+    }
+}
